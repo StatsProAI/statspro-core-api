7079a1b5cefe1cf8436896412cd45b0b33cc99b0
venturirodrigo <venturi@statspro.ai>
2025-05-13 14:18:36 -0300

    add clerk user me

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('StatsPro API')
+    .setDescription('API para o projeto StatsPro')
+    .setVersion('1.0')
+    .addBearerAuth() // Se você estiver usando autenticação JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../authentication/decorators/current-user.decorator';
+import { User } from '@clerk/backend';
+import { Public } from '../authentication/decorators/public.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -8,5 +11,15 @@ export class UsersController {
   @Get()
   async getAllUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @Get('me')
+  async getProfile(@CurrentUser() user: User) {
+    return {
+      id: user.id,
+      email: user.emailAddresses[0].emailAddress,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
   }
 }
