42b4c712a2eabc3b98d80c837f6c96d2ea7c846b
venturirodrigo <venturi@statspro.ai>
2025-05-23 15:41:22 -0300

    feat: create api token

--- src/authentication/clerk.strategy.ts
+++ src/authentication/clerk.strategy.ts
@@ -23,38 +23,10 @@ export class ClerkStrategy extends PassportStrategy(Strategy, 'clerk') {
     if (!token) {
       throw new UnauthorizedException('No token provided');
     }
-
-    const [header, payload] = token.split('.');
-
-    const decodedHeader = JSON.parse(
-      Buffer.from(header, 'base64').toString('utf-8'),
-    );
-    const decodedPayload = JSON.parse(
-      Buffer.from(payload, 'base64').toString('utf-8'),
-    );
-
-    console.log(decodedHeader);
-    console.log(decodedPayload);
-
+    
     try {
-      const tokenPayload = await verifyToken(token, {
-        secretKey: this.configService.get('CLERK_SECRET_KEY'),
-        jwtKey: `
-          -----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6FFJn7T9ZSQNUNFVhPgc
-0vmwF7u3vhmS932hZe6D92DaNkSBPjqXr1h0iK/U5x398jMwiKwIE/MMQKhG5Wfg
-XeRCcaHy4dsKThgioe2JmruXaqRgHT0AXSxrt/T4+Km44Sl84sbs4r3mW+rQdqM4
-LhgEc/xKlsjGoQ8G2I9kaGJ4RPwK0TV7Byjrz0Us3vcLl6SthBpiZpx39dDND2ON
-7752CBkUECfQ2WBvZ9ucdvAHYbAuuOX1OrXz73MscoQqCASyaPeplChIjOCly7yA
-R83sbSMTTfluVcv07X1UyjyIba4dR6zax4K1tdTO4EMeTO3iHdtFNcnPe0CpgeyP
-cwIDAQAB
------END PUBLIC KEY-----
-        `,
-        skipJwksCache: true,
-      });
-
+      const tokenPayload = await verifyToken(token, {});
       const user = await this.usersService.getUser(tokenPayload.sub);
-
       return user;
     } catch (error) {
       console.error(error);
